2a73a8d11046a54ef7fc3b202e12d06ee80cf650
IdrisseZonard <43299560+IdrisseZonard@users.noreply.github.com>
2018-09-22 16:44:24 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -18,7 +18,7 @@ bot.on('message', message => {
         .setFooter(message.author.username, message.author.avatarURL)
         .setTimestamp()
         .setColor("E26302") //http://www.code-couleur.com
-        .addField(":hammer_pick: Modérateur \n- Ban | Utilisation @ban @user raison \n- Kick | Utilisation @kick @user \n- Clear | Utilisation @clear nombre", ".")
+        .addField(":hammer_pick: Modérateur \n \n- Ban | Utilisation @ban @user raison \n- Kick | Utilisation @kick @user \n- Clear | Utilisation @clear nombre", ".")
         .addField(".", ".")
         message.channel.send(help_embed)
 }
