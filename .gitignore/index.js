b33a6e446a81b2b973dd23ae9b083a507df9d38c
IdrisseZonard <43299560+IdrisseZonard@users.noreply.github.com>
2018-09-22 16:17:06 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ bot.on('ready', function() {
     console.log("Bot lancé | Idrisse");
 });
 
-bot.login("process.env.TOKEN");
+bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
     if (message.content === prefix + "help") {
